6d9ef97b2c7d5e90c3e41c91e6ff341fda1a4400
AlexMuia31 <alexmuia31@gmail.com>
2026-05-19 15:39:00 +0300

    done auth

--- src/app/(public)/sign-up.tsx
+++ src/app/(public)/sign-up.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { SignUp } from "../../../components/clerk/SignUp";
 
 export default function SignUpScreen() {
-  return <SignUp />;
+  return <SignUp signInUrl="/" />;
 }
